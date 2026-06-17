c712b1cc8ead1d09debd2807676b28c23f1542eb
steve-ongera <gadafisteve001@gmail.com>
2026-06-17 14:40:24 +0300

    made updates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,14 +30,29 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
+ 
+THIRD_PARTY_APPS = [
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
+    "corsheaders",
+    "django_ratelimit",
+]
+ 
+LOCAL_APPS = [
+    "core",
+]
+ 
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+ 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
